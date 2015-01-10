8bf7b23e709ec2a140162255344db2f07e67b63c
EasierChutoy <liamputnam@gmail.com>
2015-01-09 21:34:14 -0800

    Added TorBE and Translate modules

--- server/grapevine.py
+++ server/grapevine.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import json
 import requests
+import TorBE
+import Translate
 
 app = Flask(__name__,static_url_path='')
 
@@ -12,8 +14,9 @@ def respond_to_request():
 	news = []
 	if(country_codes):
 		for country in country_codes:
-			translated_search_term = translate_into_local_language(search_term, country)
-			news.append(make_request_thru_tor(translated_search_term, country))
+			translated_search_term = Translate.translate_into_local_language(search_term, country)
+			url = 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%s' % translated_search_term
+			news.append(TorBE.make_request_thru_tor(url, country))
 	return "hello world"
 #	return construct_carousel(news)
 
