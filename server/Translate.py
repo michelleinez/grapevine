fce72b66894d15842cf5b6dd7e68b7a85ad265e8
EasierChutoy <liamputnam@gmail.com>
2015-01-10 14:52:46 -0800

    little fixes

--- server/Translate.py
+++ server/Translate.py
@@ -1,9 +1,10 @@
 import json
 import requests
 
+API_KEY = 'AIzaSyD7KOKsTnSnOfQO5yO4dPdGTDmipxKvbh4'
+
+# a list of languages can be found here: https://cloud.google.com/translate/v2/using_rest
 def translate_into_local_language(search_query, country_code):
-	api_key      = 'AIzaSyD7KOKsTnSnOfQO5yO4dPdGTDmipxKvbh4'
-#	search_query = 'hello world'
 	search_term  = search_query.replace(" ", "%20")
 	translate    = 'https://www.googleapis.com/language/translate/v2?key=%s&q=%s&source=en&target=%s' % (api_key, search_term, country_code)
 	response     = json.loads(requests.get(translate).text)
